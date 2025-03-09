30147c01c796d2abf441f50da5f0aafba7f8d19e
pourzarrabi <pourzarrabi@gmail.com>
2025-03-09 17:44:42 +0330

    implement imagekit

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,11 @@
 const config = {
   env: {
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    imagekit: {
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    },
     prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
