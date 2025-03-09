352a63a3b302ac8b7b2f5380cd785e8dcd13cac8
pourzarrabi <pourzarrabi@gmail.com>
2025-03-10 02:36:22 +0330

    implement qstash workflow

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,12 @@
 const config = {
   env: {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
-    prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     databaseUrl: process.env.DATABASE_URL!,
     upstash: {
       redisUrl: process.env.UPSTASH_REDIS_URL!,
@@ -14,6 +14,7 @@ const config = {
       qstashUrl: process.env.QSTASH_URL!,
       qstashToken: process.env.QSTASH_TOKEN!,
     },
+    resendToken: process.env.RESEND_TOKEN!,
   },
 };
 
